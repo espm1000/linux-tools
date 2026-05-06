5c9e14e601413100e7bcd8de5a5ecdf50ff43a5c
Nick <esp.m1k@gmail.com>
2026-05-06 17:58:43 -0500

    feat(menu): implement menu display and selection handling

--- nixtools/main.go
+++ nixtools/main.go
@@ -1,27 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"linux-tools/nixtools/pkg"
 	"log"
+	"log/slog"
 )
 
 func main() {
-	if err := runTools(); err != nil {
+	selection := pkg.DisplayMenu()
+	if err := runTools(selection); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func runTools() error {
-	// verbose := os.Getenv("NIX_VERBOSE")
-	cfg, err := pkg.GenerateConfig()
-	if err != nil {
-		return err
+func routeRequest(s string) error {
+	switch s {
+	case "1":
+		fmt.Println("option 1")
+	case "2":
+		fmt.Println("option 2")
+	case "3":
+		fmt.Println("option 3")
 	}
-	if err := pkg.UpdateEnvironmentFile(cfg); err != nil {
-		return err
+	return nil
+}
+
+func runTools(s string) error {
+	switch s {
+	case "1":
+		cfg, err := pkg.GenerateConfig()
+		if err != nil {
+			return err
+		}
+		if err := pkg.UpdateEnvironmentFile(cfg); err != nil {
+			return err
+		}
+		if err := pkg.InstallAptDependencies(cfg); err != nil {
+			return err
+		}
+	case "2":
+		slog.Info("option 2 noop")
+	case "3":
+		slog.Info("option 3 noop")
+	default:
+		slog.Error("option required")
 	}
-	if err := pkg.InstallAptDependencies(cfg); err != nil {
-		return err
-	}
+
 	return nil
 }
