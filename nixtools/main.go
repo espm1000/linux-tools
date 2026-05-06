4947160720ff4e1105a209011bcd137ede3f932a
espm1000 <esp.m1k@gmail.com>
2026-05-06 18:09:19 -0500

    feat(menu): implement menu display and selection handling (#23)
    * feat(menu): implement menu display and selection handling
    
    * refactor(menu): update DisplayMenu to return error for input handling
    
    * fix(main): handle error from DisplayMenu in main function

--- nixtools/main.go
+++ nixtools/main.go
@@ -3,25 +3,39 @@ package main
 import (
 	"linux-tools/nixtools/pkg"
 	"log"
+	"log/slog"
 )
 
 func main() {
-	if err := runTools(); err != nil {
+	selection, err := pkg.DisplayMenu()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := runTools(selection); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func runTools() error {
-	// verbose := os.Getenv("NIX_VERBOSE")
-	cfg, err := pkg.GenerateConfig()
-	if err != nil {
-		return err
+func runTools(s string) error {
+	switch s {
+	case "1":
+		cfg, err := pkg.GenerateConfig()
+		if err != nil {
+			return err
+		}
+		if err := pkg.UpdateEnvironmentFile(cfg); err != nil {
+			return err
+		}
+		if err := pkg.InstallAptDependencies(cfg); err != nil {
+			return err
+		}
+	case "2":
+		slog.Info("option 2 noop")
+	case "3":
+		slog.Info("option 3 noop")
+	default:
+		slog.Error("option required")
 	}
-	if err := pkg.UpdateEnvironmentFile(cfg); err != nil {
-		return err
-	}
-	if err := pkg.InstallAptDependencies(cfg); err != nil {
-		return err
-	}
+
 	return nil
 }
---
+++ nixtools/pkg/menu.go
@@ -0,0 +1,36 @@
+package pkg
+
+import (
+	"fmt"
+	"time"
+)
+
+type Menu struct {
+	Line1 string
+	Line2 string
+	Line3 string
+}
+
+func menuText() Menu {
+	m := Menu{
+		Line1: "1 --- Run Tool",
+		Line2: "2 --- Generate Configuration Report",
+		Line3: "3 --- Exit",
+	}
+
+	return m
+}
+
+func DisplayMenu() (string, error) {
+	var selection string
+	text := menuText()
+	fmt.Println(text.Line1)
+	fmt.Println(text.Line2)
+	fmt.Println(text.Line3)
+	fmt.Print("Selection: ")
+	if _, err := fmt.Scan(&selection); err != nil {
+		return "", err
+	}
+	time.Sleep(1 * time.Second)
+	return selection, nil
+}
