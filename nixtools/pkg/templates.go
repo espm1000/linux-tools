42ecef765b6a4efe29c76dbf439aa2b435553824
Nick <esp.m1k@gmail.com>
2026-05-10 20:43:02 -0500

    fix(templates): add 'vim' to BasicDependencies in GetPackageList

--- nixtools/pkg/templates.go
+++ nixtools/pkg/templates.go
@@ -7,7 +7,7 @@ import (
 
 func GetPackageList() Dependencies {
 	packages := Dependencies{
-		BasicDependencies: []string{"curl", "ca-certificates"},
+		BasicDependencies: []string{"curl", "ca-certificates", "vim"},
 		Docker:            []string{"docker-ce", "docker-ce-cli", "containerd.io", "docker-buildx-plugin", "docker-compose-plugin"},
 		DebianDev:         []string{"build-essential", "checkinstall", "libz-dev", "dh-make", "libssl-dev", "devscripts"},
 	}
